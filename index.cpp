d479fbfc96b3d7fdbf6dd67f109e40cfeb3519c8
KRGAMING2005 <jagarblixt@gmail.com>
2023-05-03 00:19:42 +0200

    pls kill me

--- index.cpp
+++ index.cpp
@@ -18,7 +18,9 @@ class HopefullyAWorkingThing {
         cleanup();
     }
     private:
+    VkInstance instance;
     GLFWwindow* window;
+
     void initWindow() {
         glfwInit();
 
@@ -29,7 +31,7 @@ class HopefullyAWorkingThing {
 
     }
     void initVulkan() {
-
+        createInstance();
     }
     void mainLoop() {
         while (!glfwWindowShouldClose(window)) {
@@ -40,6 +42,35 @@ class HopefullyAWorkingThing {
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "I don't even know anymore";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
     }
 };
 
